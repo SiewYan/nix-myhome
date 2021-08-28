416db38d9b47a561c72b76cdca49f720a3680900
Siewyan Hoh 何守仁 <siew.yan.hoh@cern.ch>
2021-08-28 16:53:26 +0800

    Update shell.nix

--- bashrc/nixpkgs/shell.nix
+++ bashrc/nixpkgs/shell.nix
@@ -51,15 +51,6 @@ in {
     
     # profile (any shell)
     profileExtra = ''
-      # ~/.profile: executed by the command interpreter for login shells.                                                                                                                           
-      # This file is not read by bash(1), if ~/.bash_profile or ~/.bash_login                                                                                                                       
-      # exists.                                                                                                                                                                                     
-      # see /usr/share/doc/bash/examples/startup-files for examples.                                                                                                                                
-      # the files are located in the bash-doc package.                                                                                                                                              
-
-      # the default umask is set in /etc/profile; for setting the umask                                                                                                                             
-      # for ssh logins, install and configure the libpam-umask package.                                                                                                                             
-      #umask 022                                                                                                                                                                                    
 
       # if running bash                                                                                                                                                                             
       if [ -n "$BASH_VERSION" ]; then
@@ -78,9 +69,9 @@ in {
       if [ -d "$HOME/.local/bin" ] ; then
            PATH="$HOME/.local/bin:$PATH"
       fi
-      if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
-         . $HOME/.nix-profile/etc/profile.d/nix.sh;
-      fi
+      #if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then
+      #   . $HOME/.nix-profile/etc/profile.d/nix.sh;
+      #fi
     '';
     
     # bashrc
